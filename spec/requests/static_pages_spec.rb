2a54f89dbfc839bae08db5582580cc12a0482236
scottab <scottab@cisco.com>
2012-06-19 12:41:54 -0700

    Add content of and tests for Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,8 +19,6 @@ describe "Static pages" do
 		
 		end
 
-
-
 	describe "Help page" do
 
 		it "should have the content 'Help'" do
@@ -49,6 +47,22 @@ describe "Static pages" do
 			page.should have_selector(
 				'title',
 				:text => "Ruby on Rails Tutorial Sample App | About Us"
+				)
+			end
+		end
+	
+	describe "Contact page" do
+	
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact Us')
+			end
+			
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector(
+				'title',
+				:text => "Ruby on Rails Tutorial Sample App | Contact Us"
 				)
 			end
 		end
