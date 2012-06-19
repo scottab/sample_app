5994ecab6b8a45dd6744fc3a0eedd78d9b9a8f98
scottab <scottab@cisco.com>
2012-06-19 11:28:01 -0700

    Added tests for page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,8 +6,17 @@ describe "Static pages" do
 
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
 			end
+			
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector(
+				'title',
+				:text => "Ruby on Rails Tutorial Sample App | Home"
+				)
+			end
+		
 		end
 
 
@@ -16,7 +25,15 @@ describe "Static pages" do
 
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
+			end
+			
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector(
+				'title',
+				:text => "Ruby on Rails Tutorial Sample App | Help"
+				)
 			end
 		end
 	
@@ -24,7 +41,15 @@ describe "Static pages" do
 	
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', :text => 'About Us')
+			end
+			
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector(
+				'title',
+				:text => "Ruby on Rails Tutorial Sample App | About"
+				)
 			end
 		end
 	end
