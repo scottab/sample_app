23df16f318a512b8042b7c5bcdbf96aa25e09733
scottab <scottab@cisco.com>
2012-06-19 12:46:43 -0700

    Define a variable to hold the base name of the pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
@@ -13,7 +16,7 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			page.should have_selector(
 				'title',
-				:text => "Ruby on Rails Tutorial Sample App | Home"
+				:text => "#{base_title} | Home"
 				)
 			end
 		
@@ -30,7 +33,7 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			page.should have_selector(
 				'title',
-				:text => "Ruby on Rails Tutorial Sample App | Help"
+				:text => "#{base_title} | Help"
 				)
 			end
 		end
@@ -46,7 +49,7 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector(
 				'title',
-				:text => "Ruby on Rails Tutorial Sample App | About Us"
+				:text => "#{base_title} | About Us"
 				)
 			end
 		end
@@ -62,7 +65,7 @@ describe "Static pages" do
 			visit '/static_pages/contact'
 			page.should have_selector(
 				'title',
-				:text => "Ruby on Rails Tutorial Sample App | Contact Us"
+				:text => "#{base_title} | Contact Us"
 				)
 			end
 		end
