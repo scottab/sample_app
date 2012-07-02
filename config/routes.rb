8d691ad7ec8e9f54d32c68e1e7648a002af9bfd8
Scott Brown <scottab@cisco.com>
2012-07-02 15:53:42 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
-	get "static_pages/home"
-	get "static_pages/help"
-	get "static_pages/about"
-	get "static_pages/contact"
+	
+  get "users/new"
 
+	root to: 'static_pages#home'
+	
+	match '/help',		to: 'static_pages#help'
+	match '/about',		to: 'static_pages#about'
+	match '/contact',	to: 'static_pages#contact'
+	match '/signup',	to: 'users#new'
+
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,4 +67,3 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,72 +1,33 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
 	
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	subject { page }
   
 	describe "Home page" do
-
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Sample App')
-			end
-			
-		it "should have the right title" do
-			visit '/static_pages/home'
-			page.should have_selector(
-				'title',
-				:text => "#{base_title} | Home"
-				)
-			end
-		
+		before { visit root_path }
+		it { should have_selector( 'h1',	text: 'Sample App') }
+		it { should have_selector( 'title',	text: "#{base_title}" ) }
+		it { should_not have_selector('title', text: '| Home') }
 		end
 
 	describe "Help page" do
-
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('h1', :text => 'Help')
-			end
-			
-		it "should have the right title" do
-			visit '/static_pages/help'
-			page.should have_selector(
-				'title',
-				:text => "#{base_title} | Help"
-				)
-			end
+		before { visit help_path }
+		it { should have_selector( 'h1', 	text: 'Help') }
+		it { should have_selector( 'title',	text: "#{base_title} | Help" ) }
 		end
 	
 	describe "About page" do
-	
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
-			end
-			
-		it "should have the right title" do
-			visit '/static_pages/about'
-			page.should have_selector(
-				'title',
-				:text => "#{base_title} | About Us"
-				)
-			end
+		before { visit about_path }	
+		it { should have_selector( 'h1',	text: 'About Us') }
+		it { should have_selector( 'title',	text: "#{base_title} | About Us" ) }
 		end
 	
 	describe "Contact page" do
-	
-		it "should have the content 'Contact Us'" do
-			visit '/static_pages/contact'
-			page.should have_selector('h1', :text => 'Contact Us')
-			end
-			
-		it "should have the right title" do
-			visit '/static_pages/contact'
-			page.should have_selector(
-				'title',
-				:text => "#{base_title} | Contact Us"
-				)
-			end
+		before { visit contact_path }	
+		it { should have_selector( 'h1',	text: 'Contact Us') }
+		it { should have_selector( 'title',	text: "#{base_title} | Contact Us" ) }
 		end
+
 	end
